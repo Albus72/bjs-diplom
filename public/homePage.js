0618b17803745b182225d1a44a1b666c923fc923
Albus72 <albus72@tut.by>
2022-03-22 13:58:37 +0300

    Исправлено, отформатировано

--- public/homePage.js
+++ public/homePage.js
@@ -6,71 +6,82 @@ const moneyManagerInstanse = new MoneyManager();
 const favoritesWidgetInstanse = new FavoritesWidget();
 
 const checkLogoutStatus = (status) => {
-    if (status) {
-        location.reload()
-    }
+  if (status) {
+    location.reload();
+  }
 };
 
 logoutButtonInstanse.action = () => {
-    ApiConnector.logout((response) => checkLogoutStatus(response))
+  ApiConnector.logout((response) => checkLogoutStatus(response));
 };
 
 const showUserInfo = (response) => {
-    if (response.success) {
-        ProfileWidget.showProfile(response.data);
-    }
+  if (response.success) {
+    ProfileWidget.showProfile(response.data);
+  }
 };
 
-ApiConnector.current(response => showUserInfo(response));
+ApiConnector.current((response) => showUserInfo(response));
 
 const showStocks = (response) => {
-    if (response.success) {
-        ratesInstanse.clearTable();
-        ratesInstanse.fillTable(response.data);
-    }
-}
+  if (response.success) {
+    ratesInstanse.clearTable();
+    ratesInstanse.fillTable(response.data);
+  }
+};
 
-const updateStocks = () => ApiConnector.getStocks((response) => showStocks(response));
+const updateStocks = () =>
+  ApiConnector.getStocks((response) => showStocks(response));
 updateStocks();
 setInterval(() => updateStocks(), 60000);
 
 const addMoneyHandler = (response) => {
-    console.log(response)
-    if (response.success) {
-        ProfileWidget.showProfile(response.data);
-    }
-    moneyManagerInstanse.setMessage(response.success, response.error || 'Успех!');
+  console.log(response);
+  if (response.success) {
+    ProfileWidget.showProfile(response.data);
+  }
+  moneyManagerInstanse.setMessage(
+    response.success,
+    response.error || 'Операция завершена успешно'
+  );
 };
 
 moneyManagerInstanse.addMoneyCallback = (data) => {
-    ApiConnector.addMoney(data, (response) => addMoneyHandler(response));
+  ApiConnector.addMoney(data, (response) => addMoneyHandler(response));
 };
 
 moneyManagerInstanse.conversionMoneyCallback = (data) => {
-    ApiConnector.convertMoney(data, (response) => addMoneyHandler(response));
+  ApiConnector.convertMoney(data, (response) => addMoneyHandler(response));
 };
 
 moneyManagerInstanse.sendMoneyCallback = (data) => {
-    ApiConnector.transferMoney(data, (response) => addMoneyHandler(response));
+  ApiConnector.transferMoney(data, (response) => addMoneyHandler(response));
 };
 
 const showFavorites = (response, showMessage) => {
-    if (response.success) {
-        favoritesWidgetInstanse.clearTable();
-        favoritesWidgetInstanse.fillTable(response.data);
-        moneyManagerInstanse.updateUsersList(response.data);
-    }
-    if (showMessage) {
-        favoritesWidgetInstanse.setMessage(response.success, response.error || 'Успех!')
-    }
+  if (response.success) {
+    favoritesWidgetInstanse.clearTable();
+    favoritesWidgetInstanse.fillTable(response.data);
+    moneyManagerInstanse.updateUsersList(response.data);
+  }
+  if (showMessage) {
+    favoritesWidgetInstanse.setMessage(
+      response.success,
+      response.error || 'Операция завершена успешно'
+    );
+  }
 };
 
 ApiConnector.getFavorites((response) => showFavorites(response, false));
 
 favoritesWidgetInstanse.addUserCallback = (data) => {
-    ApiConnector.addUserToFavorites(data, (response) => showFavorites(response, true));
+  ApiConnector.addUserToFavorites(data, (response) =>
+    showFavorites(response, true)
+  );
 };
 
 favoritesWidgetInstanse.removeUserCallback = (data) => {
-    ApiConnector.removeUserFromFavorites(data, (response) => showFavorites(response, true));
+  ApiConnector.removeUserFromFavorites(data, (response) =>
+    showFavorites(response, true)
+  );
 };
